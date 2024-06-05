48cf7be00f7a3f043838fba50c814157b70745ee
nguyentk31 <tkn3101@gmail.com>
2024-06-05 15:46:36 +0700

    auto delete fireball when out of display screen

--- Mario/Object-Fireball.cpp
+++ Mario/Object-Fireball.cpp
@@ -1,5 +1,6 @@
 #include "Object-Fireball.h"
 #include "Mario.h"
+#include "PlayScene.h"
 
 
 void CFireball::GetBoundingBox(float& l, float& t, float& r, float& b)
@@ -12,6 +13,19 @@ void CFireball::GetBoundingBox(float& l, float& t, float& r, float& b)
 
 void CFireball::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
 {
+	// Check if the fireball is out of the screen, then delete it
+	float cam_x, cam_y;
+	CGame *game = CGame::GetInstance();
+	game->GetCamPos(cam_x, cam_y);
+	float display_min_x = cam_x - 32;
+	float display_min_y = cam_y - 32;
+	float display_max_x = cam_x + game->GetBackBufferWidth() + 32;
+	float display_max_y = cam_y + game->GetBackBufferHeight() + 32;
+	if (x < display_min_x || x > display_max_x || y < display_min_y || y > display_max_y) {
+		isDeleted = true;
+		return;
+	}
+
 	x += vx * dt;
 	y += vy * dt;
 }
